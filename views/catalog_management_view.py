bb08dd3d9f8f614b7393a0976dac59ec4d975896
Gabriel Jacquers <gabrieljacquers@gmail.com>
2024-08-23 23:37:37 -0300

    trabajando en la vista de gestion de catalogo

--- views/catalog_management_view.py
+++ views/catalog_management_view.py
@@ -16,4 +16,17 @@ class catalog_magement_view():
         lbl_welcome_message = ttkbootstrap.Label(catalog_magement_view_frame,text="Administracion de obras literarias", font='Helvetica')
         lbl_welcome_message.place(x=400 , y= 30)
 
+        frame_to_show_results=ttk.Treeview(catalog_magement_view_frame,columns=('id','titulo','editorial','cantidad'))
+        frame_to_show_results.heading('#0',text='id')
+        frame_to_show_results.heading('#1',text='Titulo')
+        frame_to_show_results.heading('#2',text='Editorial')
+        frame_to_show_results.heading('#3',text='Cantidad')
+
+        frame_to_show_results.column('#0',width=80, minwidth=80) #Arreglar: las etiquetas del cuadro no deben poder achicarse
+        frame_to_show_results.column('#1',width=120, minwidth=120)
+        frame_to_show_results.column('#2',width=120, minwidth=120)
+        frame_to_show_results.column('#3',width=80, minwidth=80)
+        frame_to_show_results.place(x=10 , y=120 , width='400', height='300')
+
+
         return catalog_magement_view_frame

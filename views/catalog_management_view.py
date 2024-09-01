4d5ee3a0295cc856926088b91f7d520545d75cdf
Gabriel Jacquers <gabrieljacquers@gmail.com>
2024-08-31 23:16:34 -0300

    diseñando vista 'manejo de obras'

--- views/catalog_management_view.py
+++ views/catalog_management_view.py
@@ -10,23 +10,25 @@ class catalog_magement_view():
         self.main_window=contenedor
     
     def catalog_management_view(self):
+      
+        def search_frame(): 
+            frame_to_show_results=ttk.Treeview(catalog_magement_view_frame,columns=('id','titulo','editorial','cantidad'))
+            frame_to_show_results.heading('#0',text='id')
+            frame_to_show_results.heading('#1',text='Titulo')
+            frame_to_show_results.heading('#2',text='Editorial')
+            frame_to_show_results.heading('#3',text='Cantidad')
+
+            frame_to_show_results.column('#0',width=80, minwidth=80) #Arreglar: las etiquetas del cuadro no deben poder achicarse
+            frame_to_show_results.column('#1',width=120, minwidth=120)
+            frame_to_show_results.column('#2',width=120, minwidth=120)
+            frame_to_show_results.column('#3',width=80, minwidth=80)
+            frame_to_show_results.place(x=10 , y=120 , width='400', height='300')
+       
+
         catalog_magement_view_frame=ttkbootstrap.Frame(self.main_window, border=5, bootstyle='ligth')
         catalog_magement_view_frame.configure(width="990",height="560")
-
-        lbl_welcome_message = ttkbootstrap.Label(catalog_magement_view_frame,text="Administracion de obras literarias\n AÚN EN CONSTRUCCIÓN", font='Helvetica')
-        lbl_welcome_message.place(x=400 , y= 30)
-
-        frame_to_show_results=ttk.Treeview(catalog_magement_view_frame,columns=('id','titulo','editorial','cantidad'))
-        frame_to_show_results.heading('#0',text='id')
-        frame_to_show_results.heading('#1',text='Titulo')
-        frame_to_show_results.heading('#2',text='Editorial')
-        frame_to_show_results.heading('#3',text='Cantidad')
-
-        frame_to_show_results.column('#0',width=80, minwidth=80) #Arreglar: las etiquetas del cuadro no deben poder achicarse
-        frame_to_show_results.column('#1',width=120, minwidth=120)
-        frame_to_show_results.column('#2',width=120, minwidth=120)
-        frame_to_show_results.column('#3',width=80, minwidth=80)
-        frame_to_show_results.place(x=10 , y=120 , width='400', height='300')
+ 
         
-
+        btn_test=ttkbootstrap.Button(catalog_magement_view_frame , text='Test', command= search_frame )
+        btn_test.place(x=10 , y= 20)
         return catalog_magement_view_frame

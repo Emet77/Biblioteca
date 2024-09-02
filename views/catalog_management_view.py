feded799e6a10e5f393ccf471efbbc6844a1417f
Gabriel Jacquers <gabrieljacquers@gmail.com>
2024-09-01 23:43:05 -0300

    trabajando vista catalogo

--- views/catalog_management_view.py
+++ views/catalog_management_view.py
@@ -9,26 +9,11 @@ class catalog_magement_view():
     def __init__(self, contenedor) -> None:
         self.main_window=contenedor
     
-    def catalog_management_view(self):
-      
-        def search_frame(): 
-            frame_to_show_results=ttk.Treeview(catalog_magement_view_frame,columns=('id','titulo','editorial','cantidad'))
-            frame_to_show_results.heading('#0',text='id')
-            frame_to_show_results.heading('#1',text='Titulo')
-            frame_to_show_results.heading('#2',text='Editorial')
-            frame_to_show_results.heading('#3',text='Cantidad')
+    def catalog_management_view(self ):
+        big_frame=ttkbootstrap.Frame(self.main_window, border=5, bootstyle='ligth' )
+        big_frame.configure(width="990",height="560")
 
-            frame_to_show_results.column('#0',width=80, minwidth=80) #Arreglar: las etiquetas del cuadro no deben poder achicarse
-            frame_to_show_results.column('#1',width=120, minwidth=120)
-            frame_to_show_results.column('#2',width=120, minwidth=120)
-            frame_to_show_results.column('#3',width=80, minwidth=80)
-            frame_to_show_results.place(x=10 , y=120 , width='400', height='300')
-       
+        litle_frame_options=ttkbootstrap.LabelFrame(big_frame, text='Frame Chiquito' , width=150, height=560)
+        litle_frame_options.place(x=0 , y=0)
 
-        catalog_magement_view_frame=ttkbootstrap.Frame(self.main_window, border=5, bootstyle='ligth')
-        catalog_magement_view_frame.configure(width="990",height="560")
- 
-        
-        btn_test=ttkbootstrap.Button(catalog_magement_view_frame , text='Test', command= search_frame )
-        btn_test.place(x=10 , y= 20)
-        return catalog_magement_view_frame
+        return big_frame

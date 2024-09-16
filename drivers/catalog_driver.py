b33da78397876f6e829ee2120bd4e272bb387ad0
Gabriel Jacquers <gabrieljacquers@gmail.com>
2024-09-16 17:13:47 -0300

    se delego en el controlador las funciones de buscar portada y leer archivo de resumen

--- drivers/catalog_driver.py
+++ drivers/catalog_driver.py
@@ -19,45 +19,62 @@ class catalog_driver():
     def buscar_obra_catalogo(self, criterio_busqueda, donde_buscar):
         match donde_buscar:
             case 'Titulo':
-                consulta=f"""SELECT obraliteraria.titulo, obraliteraria.autor, obraliteraria.editorial,obraliteraria.id_obra,obraliteraria.portada
+                consulta=f"""SELECT obraliteraria.titulo, obraliteraria.autor, obraliteraria.editorial,obraliteraria.id_obra
                         FROM obraliteraria
                         WHERE obraLiteraria.titulo LIKE '%{criterio_busqueda}%' 
                         ORDER BY obraLiteraria.titulo;"""
 
             case 'Autor':
-                consulta=f"""SELECT obraliteraria.titulo, obraliteraria.autor, obraliteraria.editorial,obraliteraria.id_obra,obraliteraria.portada
+                consulta=f"""SELECT obraliteraria.titulo, obraliteraria.autor, obraliteraria.editorial,obraliteraria.id_obra
                         FROM obraliteraria 
                         WHERE obraLiteraria.autor LIKE '%{criterio_busqueda}%' 
                         ORDER BY obraLiteraria.autor;"""
             case 'Editorial':
-                consulta=f"""SELECT obraliteraria.titulo, obraliteraria.autor, obraliteraria.editorial,obraliteraria.id_obra,obraliteraria.portada
+                consulta=f"""SELECT obraliteraria.titulo, obraliteraria.autor, obraliteraria.editorial,obraliteraria.id_obra
                             FROM obraliteraria 
                             WHERE obraLiteraria.editorial LIKE '%{criterio_busqueda}%'
                             ORDER BY obraliteraria.editorial; """
                 
             case '':
-                consulta=f"""SELECT obraliteraria.titulo, obraliteraria.autor, obraliteraria.editorial,obraliteraria.id_obra,obraliteraria.portada
+                consulta=f"""SELECT obraliteraria.titulo, obraliteraria.autor, obraliteraria.editorial,obraliteraria.id_obra
                         FROM obraliteraria
                         ORDER BY obraliteraria.titulo;""" 
                 #Agregar un where a esta consulta para que devuelva datos cuando se le da un criterio de busqueda
         resultado_busqueda=self.ejecutar_consulta(consulta)
+        #solo devolver los datos autor titulo editorial y identific
+        #delegar cantidad disponible y cantidad todal en una nueva funcion del controlador
+        #tambien delegar la busqueda de la portada
         lista_datos=[]
-        
-        for resultado in resultado_busqueda:
-            result=list(resultado)
-            consulta_disponibles=f"SELECT COUNT(ejemplar.id_ejemplar) FROM ejemplar WHERE ejemplar.id_obra_fk = {resultado[3]} AND ejemplar.disponibilidad=0; "
-            cantidad_ejemplares_disp= self.ejecutar_consulta(consulta_disponibles)
+        for obra in resultado_busqueda:
+            lista_datos.append(obra)
+        # for resultado in resultado_busqueda:
+        #     result=list(resultado)
+        #     consulta_disponibles=f"SELECT COUNT(ejemplar.id_ejemplar) FROM ejemplar WHERE ejemplar.id_obra_fk = {resultado[3]} AND ejemplar.disponibilidad=0; "
+        #     cantidad_ejemplares_disp= self.ejecutar_consulta(consulta_disponibles)
      
             
-            consulta_totales=f"SELECT COUNT(ejemplar.id_ejemplar) FROM ejemplar WHERE ejemplar.id_obra_fk = {resultado[3]};" 
-            cantidad_ejemp_total=self.ejecutar_consulta(consulta_totales)
-            result.insert(3, cantidad_ejemplares_disp[0][0])
-            result.insert(4, cantidad_ejemp_total[0][0])
+        #     consulta_totales=f"SELECT COUNT(ejemplar.id_ejemplar) FROM ejemplar WHERE ejemplar.id_obra_fk = {resultado[3]};" 
+        #     cantidad_ejemp_total=self.ejecutar_consulta(consulta_totales)
+        #     result.insert(3, cantidad_ejemplares_disp[0][0])
+        #     result.insert(4, cantidad_ejemp_total[0][0])
             
-            lista_datos.append(result)
-        pprint(lista_datos)
-        return lista_datos
+        #     lista_datos.append(result)
 
+        # pprint(lista_datos)
+        return lista_datos
+    def ejemplares_disponibles(self,id):
+        consulta_disponibles=f"SELECT COUNT(ejemplar.id_ejemplar) FROM ejemplar WHERE ejemplar.id_obra_fk = {id} AND ejemplar.disponibilidad=0; "
+        cantidad_ejemplares_disp= self.ejecutar_consulta(consulta_disponibles)
+        return cantidad_ejemplares_disp[0][0]
+    def ejemplares_totales(self,id):
+        consulta_totales=f"SELECT COUNT(ejemplar.id_ejemplar) FROM ejemplar WHERE ejemplar.id_obra_fk = {id};" 
+        cantidad_ejemp_total=self.ejecutar_consulta(consulta_totales)
+        return cantidad_ejemp_total[0][0]
+    def mostrar_portada(self,id):
+        consulta=f"SELECT `portada` FROM `obraliteraria` WHERE obraliteraria.id_obra={id};"
+        portada=self.ejecutar_consulta(consulta)
+        return portada[0][0]
+    
     def agregarportada_obra(self ,documento_copia,id_obra):
 
         documento_copia=documento_copia
@@ -81,8 +98,22 @@ class catalog_driver():
         # f = open("myfile.txt", "w") crea y sobreescribe si ya existe un archivo con ese nombre
         pass
     def leer_resumen(self, id_obra):
+        #consultar la bd por la informacion del texto a leer 
+        consulta=f"SELECT `resumen` FROM `obraliteraria` WHERE obraliteraria.id_obra={id_obra}; "
+        direccion=self.ejecutar_consulta(consulta)
+        if(direccion[0][0]==None):
+            # no ejecutar
+            n="Este libro no tiene Resumen  :("
+            return n
+        else:
+            # tratar de abrir
+            resumen=open(direccion[0][0])
+            leer_resumen=resumen.read()
+            resumen.close()
+            return leer_resumen
+            
 
-        pass
+        
     def guardar_datos(self, id, autor, titulo, editorial, resumen):
         #crear un archivo .txt que sirva de resumen enlazado con el numero de la obra
         actual_dir=os.getcwd()

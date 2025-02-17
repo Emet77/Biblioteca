00d3b1e5fd4a43500d6fb06a63b41d7136b0572f
Gabriel Jacquers <gabrieljacquers@gmail.com>
2025-02-17 15:37:21 -0300

    clase 'vista editar obra literaria' creada

---
+++ views/buttons_catalog_management/view_btn_edit_literary_work.py
@@ -0,0 +1,20 @@
+import os
+import tkinter
+from tkinter import messagebox
+from ttkbootstrap.dialogs import MessageDialog , Messagebox
+import ttkbootstrap.localization  
+from tkinter import filedialog
+from PIL import Image , ImageTk 
+from pprint import *
+import ttkbootstrap  as ttk
+from ttkbootstrap.constants import *
+from drivers import catalog_driver
+import ttkbootstrap.window
+
+class view_btn_edit_literary_work():
+    def __init__(self,contenedor):
+        self.main_window=contenedor
+    
+
+    def view_btn_edit_literary_work(self):
+        pass

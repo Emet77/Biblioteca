d922316daa5b8f930d61fc6fa3a97806aa77ecc7
Gabriel Jacquers <gabrieljacquers@gmail.com>
2025-03-20 12:22:40 -0300

    vista 'eliminar ejemplar' terminada

--- views/buttons_catalog_management/view_delete_one_book.py
+++ views/buttons_catalog_management/view_delete_one_book.py
@@ -21,20 +21,81 @@ class view_btn_delete_one_book():
     def frame_delete_one_book(self):
         container_frame=ttkbootstrap.Frame(self.main_window)
         container_frame.grid(row=0,column=0)
+        container_frame.grid_columnconfigure(0,weight=1)
+        
+        container_frame.grid_rowconfigure(0,weight=1)
+        container_frame.grid_rowconfigure(1,weight=1)
+        container_frame.grid_rowconfigure(3,weight=1)
+        container_frame.grid_rowconfigure(4,weight=1)
+
+
+
+
 
         frame_title=ttkbootstrap.LabelFrame(container_frame,text='',bootstyle='info')
-        frame_title.grid(row=0,column=0,sticky='ew',columnspan=3,pady=5,padx=5)
+        frame_title.grid(row=0,column=0,sticky='news',pady=5,padx=5)
         frame_title.grid_columnconfigure(0,weight=1)
         lbl_title=ttkbootstrap.Label(frame_title,text='Eliminar Ejemplar',font='Helvetica',bootstyle='dark')
         lbl_title.grid(row=0,column=0,sticky='n', padx=3,pady=3)
         
         frame_warning=ttkbootstrap.LabelFrame(container_frame, text='aviso de borrar libro',bootstyle='dark')
-        frame_warning.grid(row=0,column=0,sticky='ew',columnspan=3,pady=5,padx=5)
+        frame_warning.grid(row=1,column=0,sticky='news',columnspan=1,pady=5,padx=5)
         frame_warning.grid_columnconfigure(0,weight=1)
         text_warning=f"""Recuerde: se debe eliminar un ejemplar solamente cuando este ya no este apto para seguir siendo usado como tal
         (Faltan hojas, daños por agua,etc)"""
-        label_warning=ttkbootstrap.Label(frame_warning,text=text_warning,bootstyle='dark')
+        label_warning=ttkbootstrap.Label(frame_warning,text=text_warning,bootstyle='dark',font='Helvetica',anchor='center')
         label_warning.grid(row=0,column=0,sticky='n', padx=3,pady=3)
 
+        #<------------------frame to show information about a book xD ----------------->
+
+        frame_search_book=ttkbootstrap.LabelFrame(container_frame,text='buscar y datos libro',bootstyle='info')
+        frame_search_book.grid(row=2,column=0,sticky='news',padx=5,pady=5)
+
+        frame_search_book.grid_columnconfigure(0,weight=1)
+        frame_search_book.grid_columnconfigure(1,weight=1)
+        frame_search_book.grid_columnconfigure(2,weight=1)
+        frame_search_book.grid_columnconfigure(3,weight=1)
+
+        frame_search_book.grid_rowconfigure(0,weight=1)
+        frame_search_book.grid_rowconfigure(1,weight=1)
+        frame_search_book.grid_rowconfigure(2,weight=1)
+
+        label_number=ttkbootstrap.Label(frame_search_book,text='Numero de ejemplar: ',font='Helvetica')
+        label_number.grid(column=0,row=0)
+
+        label_author=ttkbootstrap.Label(frame_search_book,text='Autor: ',font='Helvetica')
+        label_author.grid(column=0,row=1)
+
+        label_book_title=ttkbootstrap.Label(frame_search_book,text='Titulo: ',font='Helvetica')
+        label_book_title.grid(column=0,row=2)
+
+        validate_entry = lambda text: text.isdecimal()
+        var_ntry_book_number=tkinter.IntVar()
+        entry_book_number=ttkbootstrap.Entry(frame_search_book,font='Helvetica',state='normal',validate="key",validatecommand=(frame_search_book.register(validate_entry), "%S"), textvariable=var_ntry_book_number)
+        entry_book_number.grid(row=0,column=1,sticky='we',pady=3,padx=3)
+
+        label_result_author=ttkbootstrap.Label(frame_search_book,text='resultado autor',font='Helvetica')
+        label_result_author.grid(column=1,row=1)
+
+        label_result_title=ttkbootstrap.Label(frame_search_book,text='resultado titulo',font='Helvetica')
+        label_result_title.grid(column=1,row=2)
+
+        button_search=ttkbootstrap.Button(frame_search_book,text='Buscar',bootstyle='danger')
+        button_search.grid(column=2, row=0)
+
+        label_editorial=ttkbootstrap.Label(frame_search_book,text='Editorial: ',font='Helvetica')
+        label_editorial.grid(column=2,row=1)
+
+        label_result_editorial=ttkbootstrap.Label(frame_search_book,text='resultado editorial',font='Helvetica')
+        label_result_editorial.grid(column=3,row=1)
+
+
+        frame_btn_delete=ttkbootstrap.Labelframe(container_frame,text='boton! eliminar',bootstyle='danger')
+        frame_btn_delete.grid(column=0,row=3,sticky='news',padx=5,pady=5)#,columnspan=4
+        #frame_search_book.grid(row=2,column=0,sticky=EW,padx=5,pady=5)
+
+        button_delete=ttkbootstrap.Button(frame_btn_delete,text='¿Eliminar?',bootstyle='danger')
+        button_delete.grid(column=0, row=0,sticky='n',padx=5,pady=5)
+        print('filas y column en delete',frame_btn_delete.grid_size())
 
         return container_frame
